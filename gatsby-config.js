e4fd3d57f412cb0b51c4ba930a43a6f954f5cd93
arekku <jose_alex170702@hotmail.com>
2023-02-24 14:45:53 -0600

    home in work

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,23 @@
  */
 module.exports = {
   plugins: [
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Poppins`,
+            file: `https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;700;900&display=swap`,
+          },
+          {
+            name: `Text Me One`,
+            file: `https://fonts.googleapis.com/css2?family=Text+Me+One&display=swap`,
+          },
+        ],
+      },
+    },
   ],
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+
+
 
 const HomeInfo = () => {
   return (
     <>
-        <div className="containerColumn">
-            <div className="containerLeft">
-                <img src="./fotomia.jpg" alt="" srcset="" />
-            </div>
-            <div className="containerRight">
-
-            </div>
+      <div className="containerColumn">
+        <div className="containerLeft">
+          <img src="./fotomia.svg" alt="Foto alex" />
         </div>
+        <div className="containerRight">
+          <div className="topInfo">
+            <div className="titleHome">
+              <p>Hello World!</p>
+              <div className="socialMedias">
+                <FontAwesomeIcon icon={faGithub} />
+                <FontAwesomeIcon icon={faLinkedinIn} />
+              </div>
+            </div>
+            <div className="nameHome">
+              <p>I´m Alex Perez Arenas</p>
+              <p className="profesion">Frontend Developer | ONE and ISIC Student</p>
+            </div>
+          </div>
+          <div className="aboutmeHome">
+            Passionate to design and development web pages, desktop and mobile apps. Always learning things news about programming and undertaking business of products.
+          </div>
+        </div>
+      </div>
     </>
   )
 }
