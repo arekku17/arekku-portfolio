39cf215b45adb4f346f223617badb430ad4ebcb2
Arekku <jose_alex170702@hotmail.com>
2023-02-26 22:27:01 -0600

    intro finished

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,12 +6,23 @@ import { Skills } from "../components/Skills"
 import Qualification from "../components/Qualification"
 import Portfolio from "../components/Portfolio"
 import { useState } from "react"
+import Intro from "../components/Intro"
+import { useEffect } from "react"
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 export default function Home() {
 
   const [intro, setIntro] = useState(true);
+  const [parent] = useAutoAnimate()
 
-  return <>
+  useEffect(() => {
+    setIntro(true);
+    setTimeout(() => {
+      setIntro(false);
+    }, 3500);
+  },[])
+
+  return <div ref={parent} style={{position: "absolute"}}>
     {intro ? 
     <Intro></Intro>
     :
@@ -24,5 +35,5 @@ export default function Home() {
     </Layout>
     }
     
-  </>
+  </div>
 }
