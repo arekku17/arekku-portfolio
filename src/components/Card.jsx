3e53772ca79b7ad18591ad23c91bbb2819cd5dc3
Arekku <jose_alex170702@hotmail.com>
2023-02-28 19:12:46 -0600

    finished html

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,26 +9,27 @@ const Card = (props) => {
     return (
         <>
             <div className="cardPortfolio">
-                <img src={item.img} alt={item.name} />
-                <p className="titleCard">{item.name}</p>
-                <p>Tags:</p>
-                <div className="techs">
-                    {
-                        item.tags.map((tech, index) => 
-                            <p className="tech" id={index}>{tech}</p>
+                <div className="infoCard">
+                    <img src={item.img} alt={item.name} />
+                    <p className="titleCard">{item.name}</p>
+                    <p>Tags:</p>
+                    <div className="techs">
+                        {
+                            item.tags.map((tech, index) =>
+                                <p className="tech" id={index}>{tech}</p>
                             )
-                    }
+                        }
+                    </div>
+                    <p>Description:</p>
+                    <p className="desCard">
+                        {
+                            item.description
+                        }
+                    </p>
                 </div>
-                <p>Description:</p>
-                <p className="desCard">
-                    {
-                        item.description
-                    }
-                </p>
-
                 <div className="btns">
-                    <button className='btnCard'> <p>Demo</p><FontAwesomeIcon icon={faEye}/> </button>
-                    <button className='btnCard'> <p>Repo</p>  <FontAwesomeIcon icon={faGithub}/></button>
+                    <button className='btnCard'> <p>Demo</p><FontAwesomeIcon icon={faEye} /> </button>
+                    <button className='btnCard'> <p>Repo</p>  <FontAwesomeIcon icon={faGithub} /></button>
                 </div>
             </div>
         </>
